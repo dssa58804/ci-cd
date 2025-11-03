986cb72a0b7982a36c1fab7a64f4e07803c0c90f
Daniel Saghri <danielsaghri@gmail.com>
2025-11-03 14:27:07 +0100

    commented invariance in firstname domain primitive to test pipeline

--- InvarianceDTOandModel/Models/firstname.cs
+++ InvarianceDTOandModel/Models/firstname.cs
@@ -12,12 +12,12 @@ public class FirstName
     }
     private string ValidateFirstName(string firstname)
     {
-        if (string.IsNullOrEmpty(firstname))
+        /* if (string.IsNullOrEmpty(firstname))
             throw new ArgumentException("Firstname can't be empty");
         if (firstname.Length < 2 || firstname.Length > 20)
             throw new ArgumentException("Firstname must be between 2 and 20 characters long");
         if (!Regex.IsMatch(firstname, @"^[a-zA-ZæøåÆØÅ]+$"))
-            throw new ArgumentException("Firstname must only contain alphabetical letters");
+            throw new ArgumentException("Firstname must only contain alphabetical letters"); */
         return firstname;
     }
     public string GetFirstName()
